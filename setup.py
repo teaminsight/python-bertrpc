90dd3eac86467bdacb7bc4b51aa19992e062f36b
Jordan Bach <jordanbach@gmail.com>
2011-08-03 08:35:57 -0500

    changing bert dependency to insight-bert

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup, find_packages
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 setup(
     name = 'insight-bertrpc',
     license='MIT',
     version = __version__,
     description = 'BERT-RPC Library',
-    author = 'Team Insight',
-    author_email = 'jordan.bach@reelfx.com',
+    author = 'Michael J. Russo',
+    author_email = 'mjrusso@gmail.com',
+    maintainer = 'Jordan Bach',
+    maintainer_email='jordanbach@gmail.com',
     url = 'https://github.com/teaminsight/python-bertrpc',
     packages = ['bertrpc'],
-    install_requires = ["erlastic", "bert"],
+    install_requires = ["erlastic", "insight-bert"],
     classifiers = [
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
